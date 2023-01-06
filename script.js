992866dead6d5c5dab709ce41e47d3221222a375
Marina Paskal <paskalm@rogers.com>
2023-01-06 01:59:01 -0500

    Add fetch api

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-console.log("hi");
-console.log(window);
+let apiQuotes = [];
+
+// Show new quote
+function newQuote() {
+  // Pick a random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+}
+
+// Get Quotes from API
+async function getQuotes() {
+  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // Catch Error Here
+  }
+}
+
+// On load
+getQuotes();
